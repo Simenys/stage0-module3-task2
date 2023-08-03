7f22a5e39a5b306b6ace80aa8d563556c0339d63
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-03 15:38:03 +0300

    Task solved

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package school.mjc.stage0.module3.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int var1 = 0;
+        int var2 = 2000;
+        int var3 = 1000;
+        int var4 = 4000;
+        int var5 = 3000;
+        int var6 = 5000;
+        int var7 = 7000;
+        int var8 = 90000;
     }
 }
