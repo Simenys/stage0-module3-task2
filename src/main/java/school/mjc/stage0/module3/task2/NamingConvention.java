4eb765ee902f74b9060aa2c69d9f135019174f0a
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-03 16:12:55 +0300

    Think i found the problem 2

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package school.mjc.stage0.module3.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int var = 10;
-        int varOne = 1;
-        int varTwo = 2;
-        int varThree = 3;
+        int variable = 10;
+        int variable_one = 1;
+        int variableTwo = 2;
+        int variable_three = 3;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_FIVE = 5;
-        int varSix = 6;
-        int varSeven = 7;
-        int varEight = 8;
+        int variableSix = 6;
+        int variableSeven = 7;
+        int VariableEight = 8;
     }
 }
